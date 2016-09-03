a4e8dbac5573183d9f9ff7e0372c7f765624d981
Subhashini <SubhashiniBhuvaneswaran@users.noreply.github.com>
2016-09-03 11:30:27 +0530

    Add files via upload

---
+++ OddEven.java
@@ -0,0 +1,15 @@
+package Logical;
+
+import java.util.Scanner;
+
+public class OddEven {
+public static void main(String[] args) {
+	Scanner s=new Scanner(System.in);
+	int n=s.nextInt();
+	int b;
+	
+		System.out.println("hai"+( b=(n%2 == 0) ? 1:0));
+		
+	}
+}
+
