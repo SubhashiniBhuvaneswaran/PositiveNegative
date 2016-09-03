8cb67273887a73e5bf85189f53766eda5a330b6e
Subhashini <SubhashiniBhuvaneswaran@users.noreply.github.com>
2016-09-03 11:01:50 +0530

    Add files via upload

---
+++ PosNeg.java
@@ -0,0 +1,21 @@
+package Logical;
+
+import java.util.Scanner;
+
+public class PosNeg {
+	
+	public static void main (String[] args) throws java.lang.Exception
+		{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		if(n<0){
+			System.out.println("Negative:"+n);
+		}
+			else{
+						System.out.println("Positive:"+n);
+		
+			}
+			}
+		}
+
+
