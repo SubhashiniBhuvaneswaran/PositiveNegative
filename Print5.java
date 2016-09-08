cd763a7285602dd84f9806900eb4bb513e5545b2
Subhashini <SubhashiniBhuvaneswaran@users.noreply.github.com>
2016-09-08 12:05:21 +0530

    Add files via upload

---
+++ Print5.java
@@ -0,0 +1,13 @@
+package com.wipro.logic;
+
+public class Print5 {
+public static void main(String[] args) {
+	
+
+	String str="Hello";
+
+for (int i = 0; i < str.length(); i++) {
+	System.out.println(str);
+}
+}
+}
