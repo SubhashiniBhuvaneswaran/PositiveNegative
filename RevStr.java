700c4c0898cfa899ab0b9089413e23b434d8aedc
Subhashini <SubhashiniBhuvaneswaran@users.noreply.github.com>
2016-09-08 12:09:20 +0530

    Add files via upload

---
+++ RevStr.java
@@ -0,0 +1,10 @@
+package com.wipro.player;
+
+public class RevStr {
+	public static void main(String[] args) {
+		String str="subha";
+		String s=new StringBuffer(str).reverse().toString();
+		System.out.println(s);
+	}
+
+}
